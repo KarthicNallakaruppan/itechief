141099a4580eb2aacff5bb92c916c4f78d873f14
Karthic Nallakaruppan <karthic.nallakaruppan@FM1326.local>
2023-11-24 12:38:06 +1300

    Updated class name attribute

--- src/app/aboutus.tsx
+++ src/app/aboutus.tsx
@@ -8,7 +8,7 @@ export default function AboutUs() {
     return (
       <div className="z-10 flex md:w-8/12 items-center justify-center flex-col mx-auto md:px-0 px-12">
         <span className={`${sourceCodePro400.className} text-xl font-bold text-black mb-12`}> {`<About us/>`}</span>
-        <Image src={motto} alt="100% maximum effort" class="mb-6" width="120" height="120"/>
+        <Image src={motto} alt="100% maximum effort" className="mb-6" width="120" height="120"/>
         <span className={`${sourceCodePro400.className} text-sm text-gray-400 mb-6`}>const tedLasso = “The harder you work, the luckier you get.”</span>
         <span className="text-md md:text-center text-left font-light leading-7 text-text">Welcome to ITECHIEF, where a team of tech enthusiasts from New Zealand have come together to make a difference. We are a bunch of simple people, passionate about our dreams of helping small businesses achieve their goals by providing them with affordable branding, content, and technology solutions.
            <br/><br/> Our mission is simple – to empower small businesses with the tools and resources they need to succeed in today's digital world. We believe that every business deserves the opportunity to have a strong brand identity, engaging content, and access to cutting-edge technology, regardless of their size or budget. As a team, we take pride in our work and go the extra mile to ensure that our clients receive the best possible service. We are committed to building long-term relationships with our clients, and we work closely with them to understand their unique needs and requirements.
--- src/app/clients.tsx
+++ src/app/clients.tsx
@@ -9,9 +9,9 @@ import Image from 'next/image'; // Import the Image component
 export default function Clients() {
     return (
         <div className="md:w-8/12 w-full md:px-0 px-8 mx-auto items-center justify-items-center grid grid-cols-1 md:grid-cols-4  gap-4 mb-60">
-            <Image src={Client1} alt="" class="mb-6"/>
-            <Image src={Client2} alt="" class="mb-6"/>
-            <Image src={Client3} alt="" class="mb-6"/>
-            <Image src={Client4} alt="" class="mb-6"/>
+            <Image src={Client1} alt="" className="mb-6"/>
+            <Image src={Client2} alt="" className="mb-6"/>
+            <Image src={Client3} alt="" className="mb-6"/>
+            <Image src={Client4} alt="" className="mb-6"/>
         </div>
     )}
